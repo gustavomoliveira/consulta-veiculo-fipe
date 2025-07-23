10f6e36ac35476d0204b978cad687016e61bcf43
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2025-07-23 18:13:55 +0200

    first commit

---
+++ src/main/java/consultaveiculo/project/gustavo/consultaveiculo/ConsultaveiculoApplication.java
@@ -0,0 +1,19 @@
+package consultaveiculo.project.gustavo.consultaveiculo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaveiculoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConsultaveiculoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ src/test/java/consultaveiculo/project/gustavo/consultaveiculo/ConsultaveiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package consultaveiculo.project.gustavo.consultaveiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaveiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
